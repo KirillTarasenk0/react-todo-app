07ab5e1b8bac6d143c4ef749afad12bbb4e46bd9
Kirill Tarasenko <ktarasenko003@gmail.com>
2024-05-12 17:42:26 +0300

    CategorySelector and CategorySelectorForm components logic was finished

--- src/components/categorySelector/CategorySelector.jsx
+++ src/components/categorySelector/CategorySelector.jsx
@@ -5,17 +5,29 @@ import {useState} from "react";
 export default function CategorySelector() {
   //Позволяет выбирать категорию из списка существующих категорий или добавлять новую. Использует useState для управления формой ввода новой категории.
   const [clickCategoryButton, setClickCategoryButton] = useState(false);
+  const [inputCategoryValue, setInputCategoryValue] = useState([]);
+  const handleCategoryInput = (value) => {
+      setInputCategoryValue([...inputCategoryValue, value]);
+  };
   return (
     <>
       <div className="category__selector-container">
           <div>
-              <select name="" id=""></select>
+              <select name="" id="">
+                  {inputCategoryValue && inputCategoryValue.map((item, index) => {
+                      return <option key={index}>{item}</option>
+                  })}
+              </select>
           </div>
           <div>
               <button onClick={() => setClickCategoryButton(!clickCategoryButton)}>
                   Новая категория
               </button>
-              {clickCategoryButton && <CategorySelectorForm/>}
+              {clickCategoryButton &&
+                  <CategorySelectorForm
+                      handleCategoryInput={handleCategoryInput}
+                  />
+              }
           </div>
       </div>
     </>
--- src/components/categorySelector/categorySelectorForm/CategorySelectorForm.jsx
+++ src/components/categorySelector/categorySelectorForm/CategorySelectorForm.jsx
@@ -1,17 +1,20 @@
 import './CategorySelectorForm.css';
 import {useState} from "react";
 
-export default function CategorySelectorForm() {
-  const [clickSendButton, setClickSendButton] = useState(false);
-  const [inputCategoryValue, setInputCategoryValue] = useState('');
-  const handleCategoryInput = (event) => {
-      setInputCategoryValue(event.target.value);
+export default function CategorySelectorForm({handleCategoryInput}) {
+  const [inputValue, setInputValue] = useState('');
+  const handleInputChange = (event) => {
+      setInputValue(event.target.value);
+  };
+  const handleButtonClick = () => {
+    handleCategoryInput(inputValue);
+    setInputValue('');
   };
   return (
     <>
         <div>
-            <input type="text" placeholder="Введите новую категорию" onInput={handleCategoryInput}/>
-            <button onClick={() => setClickSendButton(!clickSendButton)}>
+            <input type="text" placeholder="Введите новую категорию" onChange={handleInputChange}/>
+            <button onClick={handleButtonClick}>
                 Отправить
             </button>
         </div>
