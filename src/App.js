2c562a869955b6f5ac93cb0f368e44ba60c9dd58
Kirill Tarasenko <ktarasenko003@gmail.com>
2024-05-12 18:28:22 +0300

    AddTodoForm component was rewritten according to Global State Management

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import './App.css';
 import {initialTodos} from "./helpers/initialTodos/initialTodos";
 import CategorySelector from "./components/categorySelector/CategorySelector";
 import AddTodoForm from "./components/addTodoForm/AddTodoForm";
+import {useState} from "react";
 
 function App() {
+  const [taskValue, setTaskValue] = useState([]);
+  const handleTaskInput = (inputValue, callback) => {
+      setTaskValue([...taskValue, inputValue]);
+      callback('');
+  };
   return (
     <>
       <div className="app__container">
-        <AddTodoForm/>
+        <AddTodoForm
+          handleTaskInput={handleTaskInput}
+        />
         <CategorySelector/>
       </div>
     </>
--- src/components/addTodoForm/AddTodoForm.jsx
+++ src/components/addTodoForm/AddTodoForm.jsx
@@ -1,21 +1,18 @@
 import './AddTodoForm.css';
 import {useState} from "react";
 
-export default function AddTodoForm() {
-    const [taskValue, setTaskValue] = useState([]);
+export default function AddTodoForm({handleTaskInput}) {
     const [inputValue, setInputValue] = useState('');
     const handleTaskInputChange = (event) => {
         setInputValue(event.target.value);
-    };
-    const handleTaskInput = () => {
-        setTaskValue([...taskValue, inputValue]);
-        setInputValue('');
     };
     return (
       <>
         <div>
             <input type="text" placeholder="Введите новую задачу" onChange={handleTaskInputChange}/>
-            <button type="submit" onClick={handleTaskInput}>Отправить</button>
+            <button type="submit" onClick={() => handleTaskInput(inputValue, setInputValue)}>
+                Отправить
+            </button>
         </div>
       </>
     );
