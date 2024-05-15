33243007b9d953ff73f6187b33064357f8d572bc
Kirill Tarasenko <ktarasenko003@gmail.com>
2024-05-15 04:27:41 +0300

    Todos filter logic was made

--- src/components/todoList/TodoList.jsx
+++ src/components/todoList/TodoList.jsx
@@ -1,13 +1,17 @@
 import './TodoList.css';
 import TodoItem from "../todoItem/TodoItem";
+import CategoryOptionMenu from "../commonComponents/categoryOptionMenu/CategoryOptionMenu";
 import {useState, useEffect} from "react";
 
-export default function TodoList({todos, setTodos}) {
+export default function TodoList({todos, setTodos, inputCategoryValue, getCurrentCategoryInput, currentCategoryInput}) {
     //- Отображает задачи, отфильтрованные по выбранной категории.
     //- Использует useMemo для оптимизации процесса фильтрации задач по категории.
     const [taskStatus, setTaskStatus] = useState('');
     const [currentItemId, setCurrentItemId] = useState(0);
     const [deleteItemId, setDeleteItemId] = useState(0);
+    const [openFilterButton, setOpenFilterButton] = useState(false);
+    const [filterButton, setFilterButton] = useState(false);
+    const [filteredTodos, setFilteredTodos] = useState([]);
     const changingNewTaskStatus = newTaskStatusValue => {
         setTaskStatus(newTaskStatusValue);
     };
@@ -24,6 +28,11 @@ export default function TodoList({todos, setTodos}) {
         });
         setTodos(newTodos);
     };
+    const filterTodosByCategory = () => {
+        let filteredTodosItems = todos.filter(item => item.category === currentCategoryInput);
+        setFilteredTodos(filteredTodosItems);
+    };
+    console.log(filteredTodos);
     useEffect(() => {
         removeElement(deleteItemId);
     }, [deleteItemId]);
@@ -31,24 +40,55 @@ export default function TodoList({todos, setTodos}) {
       <>
         <div>
             <div>
-                <button>Отфильтровать задачи</button>
+                <button onClick={() => setOpenFilterButton(!openFilterButton)}>
+                    Отфильтровать задачи
+                </button>
+            </div>
+            <div>
+                {openFilterButton &&
+                    <CategoryOptionMenu
+                        inputCategoryValue={inputCategoryValue}
+                        getCurrentCategoryInput={getCurrentCategoryInput}
+                    />
+                }
+                {openFilterButton &&
+                    <button onClick={() => {
+                        filterTodosByCategory();
+                        setFilterButton(true);
+                    }}>
+                        Фильтровать
+                    </button>
+                }
             </div>
         </div>
         <div>
-            {todos && todos.map(item => {
-                if (item.id === currentItemId) {
-                    item.completed = taskStatus;
-                }
-                return <TodoItem
-                    id={item.id}
-                    title={item.title}
-                    category={!item.category ? 'категория не выбрана' : item.category}
-                    completed={(item.completed === false) ? 'не выполнено' : item.completed}
-                    changingNewTaskStatus={changingNewTaskStatus}
-                    getCurrentItemId={getCurrentItemId}
-                    getDeleteItemId={getDeleteItemId}
-                />
-            })}
+            {filterButton ?
+                filteredTodos.map(item => (
+                    <TodoItem
+                        key={item.id}
+                        id={item.id}
+                        title={item.title}
+                        category={!item.category ? 'категория не выбрана' : item.category}
+                        completed={(item.completed === false) ? 'не выполнено' : item.completed}
+                        changingNewTaskStatus={changingNewTaskStatus}
+                        getCurrentItemId={getCurrentItemId}
+                        getDeleteItemId={getDeleteItemId}
+                    />
+                ))
+                :
+                todos.map(item => (
+                    <TodoItem
+                        key={item.id}
+                        id={item.id}
+                        title={item.title}
+                        category={!item.category ? 'категория не выбрана' : item.category}
+                        completed={(item.completed === false) ? 'не выполнено' : item.completed}
+                        changingNewTaskStatus={changingNewTaskStatus}
+                        getCurrentItemId={getCurrentItemId}
+                        getDeleteItemId={getDeleteItemId}
+                    />
+                ))
+            }
         </div>
       </>
     );
